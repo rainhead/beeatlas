1e9a2bb5640e48d3b987b101d2419bfbc06001ac
Peter Abrahamsen <rainhead@gmail.com>
2025-12-29 11:05:00 -0800

    work

--- data/gbif-backbone/taxon.sql
+++ data/gbif-backbone/taxon.sql
@@ -1,5 +1,15 @@
-COPY TO '/dev/stdout'
-SELECT taxonID, scientificName, taxonRank, kingdom, family, parentNameUsageID
-FROM read_csv('/dev/stdin')
-WHERE taxonomicStatus != 'doubtful'
-  AND kingdom IN ('Animalia', 'Plantae');
+CREATE TABLE taxon (
+  taxonID UINTEGER PRIMARY KEY,
+  scientificName VARCHAR NOT NULL,
+  taxonRank VARCHAR NOT NULL,
+  kingdom VARCHAR NOT NULL,
+  family VARCHAR,
+  parentNameUsageID UINTEGER
+);
+INSERT INTO taxon (
+  SELECT taxonID, scientificName, taxonRank, kingdom, family, parentNameUsageID
+  FROM read_csv('/dev/stdin')
+  WHERE taxonomicStatus != 'doubtful'
+  AND kingdom IN ('Animalia', 'Plantae')
+);
+COPY taxon TO '/dev/stdout' (FORMAT parquet);
